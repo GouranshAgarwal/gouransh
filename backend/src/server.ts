9be2f0cc40d8f729077b3b2df6cf3cc428c5990d
GouranshAgarwal <145385192+GouranshAgarwal@users.noreply.github.com>
2026-04-17 11:19:24 +0530

    tsconfig changed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: config.frontendUrl,
     credentials: true,
-    methods: ["GET", "POST", "OPTIONS"],
+    methods: ["*"],
     allowedHeaders: ["Content-Type"],
   })
 );
